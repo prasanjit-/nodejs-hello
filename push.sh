3677bb895aa2fe98de31144e75f9f037e54bf2c8
Prasanjit Singh <prasenjit.singh@seamless.se>
2017-02-12 19:40:01 +0530

    Updated_jsons

--- push.sh
+++ push.sh
@@ -6,3 +6,5 @@ else
    version="${1}"
 fi
 docker push mesos-dev.inframetic.com:5000/nodejs-hello:"${version}"
+ssh -t centos@mesos-dev.inframetic.com "sudo su - root -c 'docker pull mesos-dev.inframetic.com:5000/nodejs-hello:"${version}"' "
+#
