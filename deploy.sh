78640efd5584c00a4f5af3846a53ef81b6265680
Prasanjit Singh <prasenjit.singh@seamless.se>
2017-02-13 02:17:39 +0530

    New_Commit

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ else
 fi
 
 # destroy old application
-curl -X DELETE -H "Content-Type: application/json" http://${marathon}:8080/v2/apps/nodejs-hello
+curl -X DELETE -H "Content-Type: application/json" http://${marathon}:8080/v2/apps/nodejs-hello-8000
 ssh -t centos@mesos-dev.inframetic.com "sudo su - root -c '/root/devops/scripts/clean_docker' "
 
 # I know this one is ugly. But it works for now.
